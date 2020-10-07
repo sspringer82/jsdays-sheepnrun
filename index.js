b03a12d4e4085bd4a4cff02aabc32f863228d7f0
Sebastian Springer <info@sebastian-springer.com>
2020-10-07 09:17:43 +0200

    Aufgabe 1: Lösung

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello World');
+  const canvas = document.getElementById('sheep-and-run');
+  const context = canvas.getContext('2d');
+
+  const background = new Background(context);
+  const promise = background.init('assets/background1.png');
+
+  Promise.all([promise]).then(() => {
+    background.render(canvas.width, canvas.height);
+  });
 });
